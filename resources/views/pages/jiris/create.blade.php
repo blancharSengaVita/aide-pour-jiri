9ecc99abe638704b183402a3a367bfd8c6150452
Dominique Vilain <dominique.vilain@hepl.be>
2023-10-30 18:48:12 +0100

    refactor view to reuse layouts provided by breeze

--- resources/views/pages/jiris/create.blade.php
+++ resources/views/pages/jiris/create.blade.php
@@ -1,31 +1,29 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Create a new jiri</h1>
-    <form action="/jiris" method="post">
-        @csrf
-        <div>
-            <label for="name">Name</label>
-            <input type="text" name="name" id="name">
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Create a new jiri') }}
+        </h2>
+    </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <form action="/jiris" method="post">
+                @csrf
+                <div>
+                    <label for="name">Name</label>
+                    <input type="text" name="name" id="name">
+                </div>
+                <div>
+                    <label for="starting_at">Starting date and time</label>
+                    <input type="datetime-local" name="starting_at" id="starting_at">
+                </div>
+                <div>
+                    <label for="duration">Duration in minutes</label>
+                    <input type="number" name="duration" id="duration" min="1" max="480" value="1">
+                </div>
+                <div>
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">Create this jiri</button>
+                </div>
+            </form>
         </div>
-        <div>
-            <label for="starting_at">Starting date and time</label>
-            <input type="datetime-local" name="starting_at" id="starting_at">
-        </div>
-        <div>
-            <label for="duration">Duration in minutes</label>
-            <input type="number" name="duration" id="duration" min="1" max="480" value="1">
-        </div>
-        <div>
-            <button type="submit">Create this jiri</button>
-        </div>
-    </form>
-</body>
-</html>
+    </div>
+</x-app-layout>
--- resources/views/pages/jiris/index.blade.php
+++ resources/views/pages/jiris/index.blade.php
@@ -1,20 +1,20 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    @unless(empty($jiris))
-        <ul>
-            @foreach($jiris as $jiri)
-                <li>{{ $jiri->name }}</li>
-            @endforeach
-        </ul>
-    @endunless
-    <p><a href="/jiris/create">Create a new jiri</a></p>
-</body>
-</html>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Your Jiris') }}
+        </h2>
+    </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            @unless(empty($jiris))
+                <ul>
+                    @foreach($jiris as $jiri)
+                        <li>{{ $jiri->name }}</li>
+                    @endforeach
+                </ul>
+            @endunless
+            <p><a href="/jiris/create" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Create a new jiri</a></p>
+        </div>
+    </div>
+
+</x-app-layout>
