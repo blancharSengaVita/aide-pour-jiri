27cb5172c9b4097aebb73252ec8f08824ff7ce41
Dominique Vilain <dominique.vilain@hepl.be>
2023-11-10 19:31:42 +0100

    Preparing i18n before its getting boring

--- resources/views/pages/jiris/index.blade.php
+++ resources/views/pages/jiris/index.blade.php
@@ -4,16 +4,16 @@
             {{ __('Your Jiris') }}
         </h2>
     </x-slot>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <div class="flex flex-col gap-4">
             @unless(empty($jiris))
-                <ul>
+                <ul class="flex flex-col gap-4">
                     @foreach($jiris as $jiri)
-                        <li>{{ $jiri->name }}</li>
+                        <li><a href="/jiris/{{$jiri->id}}/edit/" class="underline">{{ $jiri->name }}</a></li>
                     @endforeach
                 </ul>
             @endunless
-            <p><a href="/jiris/create" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Create a new jiri</a></p>
+            <p><a href="/jiris/create" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{{ __('Create a new jiri')}}</a></p>
         </div>
     </div>
 
